c45665d7f9b814566cfa28ed03c9f21c88492a9e
fareskebeh <fares.kebbeh@gmail.com>
2025-07-12 12:23:53 -0700

    feat: Unstyled Navigation finished

--- src/app/components/Container.tsx
+++ src/app/components/Container.tsx
@@ -1,8 +1,10 @@
 
 const Container = () => {
   return (
-    <div className="sm:flex-1 sm:absolute sm:right-0 sm:left-[50%]">
-        container
+    <div className="sm:flex-1 sm:absolute sm:right-0 sm:left-[50%] *:h-screen">
+        <div id="about">About Me</div>
+        <div id="work">My Work</div>
+        <div id="contact">Contact Me</div>
     </div>
   )
 }
--- src/app/components/Nav.tsx
+++ src/app/components/Nav.tsx
@@ -1,8 +1,10 @@
 
 const Nav = () => {
   return (
-    <div className="fixed top-0 right-0 left-0">
-        Nav
+    <div className="fixed top-0 right-0 left-0 p-4 flex justify-evenly *:cursor-pointer z-90">
+        <a href="#about">ABOUT</a>
+        <a href="#work">MY WORK</a>
+        <a href="#contact">CONTACT ME</a>
     </div>
   )
 }
