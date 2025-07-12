021f1b6760680cf507318eb80be62f1423d54a52
fareskebeh <fares.kebbeh@gmail.com>
2025-07-12 13:22:41 -0700

    feat: Finished the card (no pfp yet)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Nav from "@components/Nav"
 
 const App = () => {
   return (
-    <div className="font-jetbrains flex flex-col sm:flex-row h-screen">
+    <div className="font-jetbrains **:selection:bg-white **:selection:text-black flex flex-col sm:flex-row h-screen">
       <Nav/>
       <div className="pt-12">
         <Card/>
